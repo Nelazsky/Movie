8cc25cfae764d91e4807db6af6631a66a5155251
Nelazsky <nelazsky.vladimir@gmail.com>
2020-08-12 15:39:25 +0300

    Test merging

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container mx-auto px-4 pt-16">
         <div class="popular-movies">
-            <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Popular movies</h2>
+            <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Популярные фильмы</h2>
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8 ">
                 @foreach ($popularMovies as $movie)
                     <x-movie-card :movie="$movie" :genres="$genres"></x-movie-card>
@@ -13,7 +13,7 @@
     </div>
     <div class="container mx-auto px-4 pt-16">
         <div class="popular-movies">
-            <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">New movies</h2>
+            <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Новинки</h2>
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8 ">
                 @foreach ($nowPlayingMovies as $movie)
                     <x-movie-card :movie="$movie" :genres="$genres"></x-movie-card>
